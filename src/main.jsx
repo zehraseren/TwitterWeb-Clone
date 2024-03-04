3b5e12bac5a403952f7622ec4adbce0ae15e3066
Zehra Şeren <fatmazehraseren@gmail.com>
2024-03-04 05:26:41 +0300

    Tailwind CSS kuruldu.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import routes from "./routes";
+import "./assets/css/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={routes}/>
+  <RouterProvider router={routes} />
 );
