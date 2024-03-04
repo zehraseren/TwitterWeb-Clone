d145b70691f4ce6dff3b5703989e3ca338f90be4
Zehra Şeren <fatmazehraseren@gmail.com>
2024-03-04 07:45:42 +0300

    Routing düzenlemesi yapıldı.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,25 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/home";
-import Explore from "../pages/explore";
-import Notifications from "../pages/notifications";
-import NotFound from "../pages/notFound";
+import MainLayout from "~/layouts/main";
+import Home from "~/pages/home";
+import Explore from "~/pages/explore";
+import Notifications from "~/pages/notifications";
+import NotFound from "~/pages/notFound";
 
 const routes = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/explore",
-    element: <Explore />,
-  },
-  {
-    path: "/notifications",
-    element: <Notifications />,
-  },
-  {
-    path: "*",
-    element: <NotFound />,
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "explore",
+        element: <Explore />,
+      },
+      {
+        path: "notifications",
+        element: <Notifications />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
   },
 ]);
 
