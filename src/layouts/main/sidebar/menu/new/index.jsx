69aa87ce5ff35bfb5c3b2169bbc27b8ee77d1633
Zehra Şeren <fatmazehraseren@gmail.com>
2024-03-07 04:11:32 +0300

    NewTweet component'i oluşturuldu.

---
+++ src/layouts/main/sidebar/menu/new/index.jsx
@@ -0,0 +1,9 @@
+import Button from "../../../../../components/button";
+
+export default function NewTweet() {
+  return (
+    <div className="py-[14px] w-[90%] mb-1 mt-1">
+      <Button size="large">Gönder</Button>
+    </div>
+  );
+}
