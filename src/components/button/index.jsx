bc900efc5f055e4bbe97683509c02c3ce16cd916
Zehra Şeren <fatmazehraseren@gmail.com>
2024-03-07 04:13:07 +0300

    Button component'i oluşturuldu.

---
+++ src/components/button/index.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+import classNames from "classnames";
+import { createElement } from "react";
+import PropTypes from "prop-types";
+
+export default function Button({ size, children }) {
+  return createElement(
+    "button",
+    {
+      className: classNames(
+        "bg-[#1d9bf0] rounded-full flex items-center justify-center font-bold hover:bg-[#1a8cd8] transition-colors",
+        {
+          "px-4 h-9": size === "normal",
+          "px-4 h-[52px] w-full text-[17px]": size === "large",
+        }
+      ),
+    },
+    children
+  );
+}
+
+Button.propTypes = {
+  size: PropTypes.oneOf(["normal", "large"]),
+};
+
+Button.defaultProps = {
+  size: "normal",
+};
