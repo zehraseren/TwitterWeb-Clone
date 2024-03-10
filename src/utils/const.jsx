0343f1d5e655b78ccb23f56c89d1e9717b8476e1
Zehra Şeren <fatmazehraseren@gmail.com>
2024-03-10 22:54:54 +0300

    Profil için path düzeltildi.

--- src/utils/const.jsx
+++ src/utils/const.jsx
@@ -1,3 +1,7 @@
+import store from "../store";
+
+const states = store.getState().auth.currentAccount.username;
+
 export const mainMenu = [
   // Homepage
   {
@@ -36,7 +40,7 @@ export const mainMenu = [
         </svg>
       ),
       passive: (
-        <svg viewBox="0 0 24 24" width={26.25} height={26.25}className="block">
+        <svg viewBox="0 0 24 24" width={26.25} height={26.25} className="block">
           <path
             fill="#e7e9ea"
             d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"
@@ -163,7 +167,7 @@ export const mainMenu = [
   },
   // Profile
   {
-    path: "/profile",
+    path: `/${states?.auth.currentAccount.username}`,
     title: "Profil",
     icon: {
       active: (
