a8f393fd7680b82eaddcf66eb0fc93e690450979
Zehra Şeren <fatmazehraseren@gmail.com>
2024-03-16 02:38:04 +0300

    Search ve Premium eklendi.

--- src/layouts/main/rightbar/index.jsx
+++ src/layouts/main/rightbar/index.jsx
@@ -1,5 +1,11 @@
-export default function RightBar(){
-    return(
-        <aside className="w-[350px] mr-2.5">Right</aside>
-    )
+import Premium from "./premium";
+import Search from "./search";
+
+export default function RightBar() {
+  return (
+    <aside className="w-[350px] mr-4">
+      <Search />
+      <Premium/>
+    </aside>
+  );
 }
---
+++ src/layouts/main/rightbar/premium/index.jsx
@@ -0,0 +1,7 @@
+export default function Premium() {
+  return (
+    <section className="bg-[#16181c] mb-4 py-3 px-4 rounded-2xl border border-[#16181c]">
+      Premium
+    </section>
+  );
+}
