28fa284c56b26882bd16c5ccadab0818c85abee1
Zehra Şeren <fatmazehraseren@gmail.com>
2024-03-16 02:53:17 +0300

    CSS düzenlendi.

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -8,7 +8,7 @@ export default function Button({ size, children }) {
     "button",
     {
       className: classNames(
-        "bg-[#1d9bf0] rounded-full flex items-center justify-center font-bold hover:bg-[#1a8cd8] transition-colors",
+        "bg-[#1d9bf0] rounded-full text-white flex items-center justify-center font-bold hover:bg-[#1a8cd8] transition-colors",
         {
           "px-4 h-9": size === "normal",
           "px-4 h-[52px] w-full text-[17px]": size === "large",
