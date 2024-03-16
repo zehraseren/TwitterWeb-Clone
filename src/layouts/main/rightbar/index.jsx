0bd43d3d6b18f1441125e9103ea864113d02b029
Zehra Şeren <fatmazehraseren@gmail.com>
2024-03-16 03:20:34 +0300

    Topics eklendi.

--- src/layouts/main/rightbar/index.jsx
+++ src/layouts/main/rightbar/index.jsx
@@ -1,11 +1,13 @@
 import Premium from "./premium";
 import Search from "./search";
+import Topics from "./topics";
 
 export default function RightBar() {
   return (
     <aside className="w-[350px] mr-4">
       <Search />
       <Premium/>
+      <Topics/>
     </aside>
   );
 }
