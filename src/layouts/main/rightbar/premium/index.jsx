af835772b1dc1bb8be189f467be4a5d435bb4791
Zehra Şeren <fatmazehraseren@gmail.com>
2024-03-17 09:46:21 +0300

    Premium component'i düzenlendi.

--- src/layouts/main/rightbar/premium/index.jsx
+++ src/layouts/main/rightbar/premium/index.jsx
@@ -1,7 +1,17 @@
+/* eslint-disable react/no-unescaped-entities */
+import Button from "../../../../components/button";
+
 export default function Premium() {
   return (
-    <section className="bg-[#16181c] mb-4 py-3 px-4 rounded-2xl border border-[#16181c]">
-      Premium
+    <section className="bg-[#16181c] mb-4 py-3 px-4 rounded-2xl text-[#e7e9ea] gap-2.5 flex flex-col border border-[#16181c]">
+      <h6 className="text-xl font-extrabold leading-6">Premium'a Abone ol</h6>
+      <p className="text-[16px] font-normal leading-5">
+        Yeni özellikleri açmak için abone ol ve uygun olman durumunda reklam
+        geliri payı kazan.
+      </p>
+      <div className="self-start">
+        <Button>Abone ol</Button>
+      </div>
     </section>
   );
 }
