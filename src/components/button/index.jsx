859366646eaf7534f7248f93bb0d2f7987527a98
Zehra Şeren <fatmazehraseren@gmail.com>
2024-03-18 23:51:27 +0300

    Yeni css özellikleri eklendi.

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -3,15 +3,18 @@ import classNames from "classnames";
 import { createElement } from "react";
 import PropTypes from "prop-types";
 
-export default function Button({ size, children }) {
+export default function Button({ size, children, variant }) {
   return createElement(
     "button",
     {
       className: classNames(
-        "bg-[#1d9bf0] rounded-full text-white flex items-center justify-center font-bold hover:bg-[#1a8cd8] transition-colors",
+        "rounded-full flex items-center justify-center font-bold transition-colors",
         {
+          "px-4 h-8 text-sm": size === "small",
           "px-4 h-9": size === "normal",
           "px-4 h-[52px] w-full text-[17px]": size === "large",
+          "bg-[#1d9bf0] text-white hover:bg-[#1a8cd8]": variant === "primary",
+          "bg-[#eff3f4] text-black hover:bg-[#d7dbdc]": variant === "white",
         }
       ),
     },
@@ -20,9 +23,11 @@ export default function Button({ size, children }) {
 }
 
 Button.propTypes = {
-  size: PropTypes.oneOf(["normal", "large"]),
+  size: PropTypes.oneOf(["small", "normal", "large"]),
+  variant: PropTypes.oneOf(["primary", "white"]),
 };
 
 Button.defaultProps = {
   size: "normal",
+  variant: "primary",
 };
