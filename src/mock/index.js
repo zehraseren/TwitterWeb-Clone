b319771619386289781fe853373e01176cdef5ed
Zehra Şeren <fatmazehraseren@gmail.com>
2024-03-18 23:04:04 +0300

    WhoFollowUser datası oluşturuldu.

---
+++ src/mock/index.js
@@ -0,0 +1,23 @@
+export const WhoFollowUsers = [
+  {
+    id: 1,
+    username: "MarvelStudios",
+    fullname: "Marvel Studios",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1562523305927618561/wbdOmBqz_bigger.jpg",
+  },
+  {
+    id: 2,
+    username: "MCU_Direct",
+    fullname: "MCU The Direct",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1329130767344168968/uRUYAijQ_bigger.jpg",
+  },
+  {
+    id: 3,
+    username: "Marvel",
+    fullname: "Marvel Entertainment",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1560508217867718657/8ak-Td6l_bigger.jpg",
+  },
+];
