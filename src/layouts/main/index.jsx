56a4d2244d0bd1b5c75948467ad1a9250eeab66d
Zehra Şeren <fatmazehraseren@gmail.com>
2024-03-21 16:25:46 +0300

    CSS düzenlendi.

--- src/layouts/main/index.jsx
+++ src/layouts/main/index.jsx
@@ -4,10 +4,10 @@ import RightBar from "./rightbar";
 
 export default function MainLayout() {
   return (
-    <div className="w-[1249px] mx-auto flex ">
+    <div className="w-[78.063rem] mx-auto flex ">
       <Sidebar />
-      <main className="flex flex-1 gap-[30px]">
-        <main className="flex-1 border-x border-[#2f3336] max-w-[600px]">
+      <main className="flex flex-1 gap-[1.875rem]">
+        <main className="flex flex-1 border-x border-[#2f3336] max-w-[37.5rem]">
           <Outlet />
         </main>
       </main>
