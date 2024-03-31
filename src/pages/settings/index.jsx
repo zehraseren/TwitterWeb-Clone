01a83638c1c2f74e80897ba83afa60600f720634
Zehra Şeren <fatmazehraseren@gmail.com>
2024-04-01 01:17:24 +0300

    Settings oluşturuldu.

---
+++ src/pages/settings/index.jsx
@@ -0,0 +1,14 @@
+import Sidebar from "../../layouts/main/sidebar";
+
+export default function Settings() {
+  return (
+    <div className="w-[78.063rem] mx-auto flex">
+      <Sidebar />
+      <main className="flex flex-1">
+        <main className="flex flex-1 border-x border-[#2f3336] max-w-[27.5rem]">
+          SS
+        </main>
+      </main>
+    </div>
+  );
+}
