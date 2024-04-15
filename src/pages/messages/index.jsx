c7615844160206a0170203b909b9fbb8d6025dbb
Zehra Şeren <fatmazehraseren@gmail.com>
2024-04-15 17:42:13 +0300

    Messages component'i düzenlendi.

--- src/pages/messages/index.jsx
+++ src/pages/messages/index.jsx
@@ -1,3 +1,4 @@
+import Content from "../../components/content";
 import Sidebar from "../../layouts/main/sidebar";
 
 export default function Messages() {
@@ -5,9 +6,12 @@ export default function Messages() {
     <div>
       <div className="w-[78.063rem] mx-auto flex">
         <Sidebar />
-        <main className="flex flex-1">
-          <main className="flex flex-1 border-x border-[#2f3336] max-w-[27.5rem]">SS</main>
+        <main className="flex gap-3.5">
+          <main className="border-x border-[#2f3336] w-[28rem]]">Messages</main>
         </main>
+        <div className="mr-0.5">
+          <Content />
+        </div>
       </div>
     </div>
   );
