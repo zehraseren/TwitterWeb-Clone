7c192fe1aee97fe5f02838d96ee472b4ba894cfa
Zehra Şeren <fatmazehraseren@gmail.com>
2024-04-15 17:37:11 +0300

    Routing oluşturuldu.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -9,6 +9,14 @@ import Communities from "../pages/communities";
 import Profile from "../pages/profile";
 import NotFound from "../pages/notFound";
 import Settings from "../pages/settings";
+import YourAccount from "../pages/settings/components/YourAccount";
+import Monetization from "../pages/settings/components/Monetization";
+import ManageSubscriptions from "../pages/settings/components/ManageSubscriptions";
+import Security from "../pages/settings/components/Security";
+import PrivacySafety from "../pages/settings/components/PrivacySafety";
+import NotificationSettings from "../pages/settings/components/NotificationSettings";
+import Accessibility from "../pages/settings/components/Accessibility";
+import AdditionalResources from "../pages/settings/components/AdditionalResources";
 
 const routes = createBrowserRouter([
   {
@@ -55,7 +63,39 @@ const routes = createBrowserRouter([
       },
       {
         path: "account",
-        element: <Explore />,
+        element: <YourAccount/>
+      },
+      {
+        path: "monetization",
+        element: <Monetization/>
+      },
+      {
+        path: "i/premium_sign_up",
+        element: <YourAccount/>
+      },
+      {
+        path: "manage_subscriptions",
+        element: <ManageSubscriptions/>
+      },
+      {
+        path: "security_and_account_access",
+        element: <Security/>
+      },
+      {
+        path: "privacy_and_safety",
+        element: <PrivacySafety/>
+      },
+      {
+        path: "notifications",
+        element: <NotificationSettings/>
+      },
+      {
+        path: "accessibility_display_and_languages",
+        element: <Accessibility/>
+      },
+      {
+        path: "about",
+        element: <AdditionalResources/>
       },
     ],
   },
